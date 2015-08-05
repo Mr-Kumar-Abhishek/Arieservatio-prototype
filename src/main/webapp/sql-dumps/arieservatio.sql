8314c07187f363a538f66eb43e30c31a073957d7
Abhishek Kumar <kumar.abhishek@techie.com>
2015-08-05 17:31:45 +0530

    resturctured tables

--- src/main/webapp/sql-dumps/arieservatio.sql
+++ src/main/webapp/sql-dumps/arieservatio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.3.208.130:3306
--- Generation Time: Aug 05, 2015 at 08:36 AM
+-- Generation Time: Aug 05, 2015 at 12:00 PM
 -- Server version: 5.5.41
 -- PHP Version: 5.3.3
 
@@ -23,16 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fare`
+-- Table structure for table `booked_seats`
 --
 
-CREATE TABLE IF NOT EXISTS `fare` (
-  `fare_index` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `booked_seats` (
+  `pnr` int(11) unsigned NOT NULL,
+  `seatno` int(3) unsigned NOT NULL,
+  `time_index` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`pnr`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flight_cap`
+--
+
+CREATE TABLE IF NOT EXISTS `flight_cap` (
+  `fcid` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `flightno` varchar(255) DEFAULT NULL,
-  `depcode` varchar(3) DEFAULT NULL,
-  `arrcode` varchar(3) DEFAULT NULL,
-  `time_index` int(11) NOT NULL,
-  PRIMARY KEY (`fare_index`)
+  `economy` int(3) unsigned NOT NULL,
+  `premium` int(3) unsigned NOT NULL,
+  `business` int(3) unsigned NOT NULL,
+  `first_class` int(3) unsigned NOT NULL,
+  `time_index` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`fcid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -43,7 +58,9 @@ CREATE TABLE IF NOT EXISTS `fare` (
 
 CREATE TABLE IF NOT EXISTS `flight_master` (
   `fid` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `flightno` varchar(255) DEFAULT NULL,
+  `flightno` int(11) DEFAULT NULL,
+  `airport` varchar(3) DEFAULT NULL,
+  `onfly` tinyint(1) NOT NULL,
   `capacity` int(3) unsigned NOT NULL,
   PRIMARY KEY (`fid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -61,8 +78,21 @@ CREATE TABLE IF NOT EXISTS `flight_schedule` (
   `arrcode` varchar(3) DEFAULT NULL,
   `deptime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `arrtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `base_fare` decimal(8,0) NOT NULL,
   PRIMARY KEY (`time_index`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `waiting_list`
+--
+
+CREATE TABLE IF NOT EXISTS `waiting_list` (
+  `pnr` int(11) unsigned NOT NULL,
+  `time_index` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`pnr`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
